2a1bb382b697d2b279e411caf5731f652b46684b
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-09-16 12:57:47 -0400

    burger builder added loyaout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from './components/Layout/Layout'
+
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
