c18d115953abd33d4c2604659feb3bfe8ec6d6e2
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-09-17 19:23:57 -0400

    backdrop is done

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Backdrop.module.css"
 
 const backdrops = (props) => (
-  props.show ? <div className={classes.Backdrop}></div> : null
+  props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
 
 export default backdrops;
