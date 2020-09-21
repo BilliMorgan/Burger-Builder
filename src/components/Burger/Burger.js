4c6d71f191bccc976a8dca0652b39c30dae9e784
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-09-21 19:29:35 -0400

    optimazed Model by using didMount for Order Summary'

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from "react";
 import classes from "./Burger.module.css";
+
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = (props) => {
+  
   let transformedIngredients = Object.keys(props.ingredients)
   .map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -15,7 +17,7 @@ const burger = (props) => {
   if(transformedIngredients.length === 0){
     transformedIngredients =<p> Please start adding ingredients!</p>
   }
-  console.log(transformedIngredients)
+  
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
@@ -24,5 +26,6 @@ const burger = (props) => {
     </div>
   );
 };
+
 
 export default burger;
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,42 @@
-import React from "react";
-import Aux from "../../../hoc/Aux";
+import React, { Component } from "react";
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends Component {
+  // This could be a functional component doesn't have to be a class
+  componentDidUpdate(){
+    console.log('[OrderSummary] DidUpdate')
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
